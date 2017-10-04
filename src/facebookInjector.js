00e6af39713b565cc9202d61068b11739ef945dc
Rogerio Chaves <rogeriocfj@gmail.com>
2017-10-04 10:30:17 -0300

    Insert red border on all facebook newsfeed news links

---
+++ src/facebookInjector.js
@@ -0,0 +1,43 @@
+// ex: https://l.facebook.com/l.php?u=https%3A%2F%2Ftecnoblog.net%2F224816%2Frumor-preco-iphone-8-plus-brasil%2F&h=ATPUoAPZS7_4ovGV7USWGOosDT_5NhE1bYLryDQKKfgt03fwcna46IbFp1CItisdDKszIIV5JfaDe9oifkpB2kdUpRQLF9AsnoXCjD9POpKrKYmrAb6cFjNtRbzZryhYOs7aygRS_bI-VUu8IfF801fPixhsajw9w7qFjSZjdjTQfUohtPKwS8Q-zor81wKNdqbWHUSLZcwCFrf8TDUELQdeHuwxJngRiWTz1d1W3dBYQqHA_Wcud__TVhpfjzAS2pBeYlbf4vXyH2HdfcQ6k0YrC2v4aBb1HWwDGw
+const filterLink = a => {
+  if (a.dataset.appname || a.text) return false;
+
+  return getNonFacebookUrl(a);
+};
+
+const getNonFacebookUrl = a => {
+  if (!a.href.match(/facebook\.com/)) {
+    return a.href;
+  }
+  const matches = a.href.match(/facebook\.com\/l.php\?u=(.*?)&/);
+  if (matches) {
+    return decodeURIComponent(matches[1]);
+  }
+
+  return null;
+};
+
+const putBorders = () => {
+  const storyLinks = [].filter.call(
+    document.querySelectorAll(".fbUserStory a[href]"),
+    filterLink
+  );
+
+  storyLinks.forEach(storyLink => {
+    storyLink.style.border = "5px solid red";
+  });
+};
+
+let injectTimeout;
+const injectOnFeedRefresh = () => {
+  const observer = new MutationObserver(function(mutations) {
+    clearTimeout(injectTimeout);
+    injectTimeout = setTimeout(putBorders, 100);
+  });
+
+  observer.observe(document.querySelector("[role='feed']"), {
+    subtree: true,
+    childList: true
+  });
+};
+injectOnFeedRefresh();
