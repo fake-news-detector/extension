efc9d2f8c8c6cedf98767e841f27ea65434848f3
Rogerio Chaves <rogeriocfj@gmail.com>
2017-10-04 18:14:14 -0300

    Add chokidar for better dev flow and add elm style-elements for styling

--- src/Main.elm
+++ src/Main.elm
@@ -1,19 +1,55 @@
 module Main exposing (..)
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
+import Color exposing (..)
+import Element exposing (..)
+import Element.Attributes exposing (..)
+import Html exposing (Html)
+import Html.Attributes
+import Style exposing (..)
+import Style.Border as Border
+import Style.Color as Color
+
+
+type MyStyles
+    = NoStyle
+    | Button
+    | VoteCount
+
+
+stylesheet : Style.StyleSheet MyStyles variation
+stylesheet =
+    styleSheet
+        [ style Button
+            [ Color.text (rgb 75 79 86)
+            , Color.background (rgb 246 247 249)
+            , Border.all 1
+            , Color.border (rgb 75 79 86)
+            ]
+        , style VoteCount
+            [ Color.text white
+            , Color.background (rgba 0 0 0 0.8)
+            , Border.rounded 6
+            ]
+        ]
 
 
 main : Html msg
 main =
-    div
-        [ style
+    Html.div
+        [ Html.Attributes.style
             [ ( "position", "absolute" )
             , ( "right", "0" )
             , ( "z-index", "1" )
-            , ( "border", "10px solid red" )
-            , ( "color", "white" )
             ]
         ]
-        [ text "Bullshit!"
+        [ Element.layout stylesheet view
+        ]
+
+
+view : Element MyStyles variation msg
+view =
+    column NoStyle
+        [ spacing 5, padding 5 ]
+        [ button Button [ padding 4 ] (text "🏴 Sinalizar")
+        , el VoteCount [ padding 6 ] (text "\x1F916 60% click bait")
         ]
