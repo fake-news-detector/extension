fdc60b2eeb2aacdb415402d1af90f1f106ed5499
Rogerio Chaves <rogeriocfj@gmail.com>
2017-10-05 23:27:35 -0300

    Submit vote to server and update newstory

---
+++ src/Category.elm
@@ -0,0 +1,88 @@
+module Category exposing (..)
+
+
+type Category
+    = Legitimate
+    | FakeNews
+    | ClickBait
+    | ExtremelyBiased
+    | Satire
+
+
+fromId : Int -> Category
+fromId id =
+    case id of
+        1 ->
+            Legitimate
+
+        2 ->
+            FakeNews
+
+        3 ->
+            ClickBait
+
+        4 ->
+            ExtremelyBiased
+
+        5 ->
+            Satire
+
+        _ ->
+            Legitimate
+
+
+toId : Category -> Int
+toId category =
+    case category of
+        Legitimate ->
+            1
+
+        FakeNews ->
+            2
+
+        ClickBait ->
+            3
+
+        ExtremelyBiased ->
+            4
+
+        Satire ->
+            5
+
+
+toEmoji : Category -> String
+toEmoji category =
+    case category of
+        Legitimate ->
+            "✅"
+
+        FakeNews ->
+            "💀"
+
+        ClickBait ->
+            "🐟"
+
+        ExtremelyBiased ->
+            "🙉"
+
+        Satire ->
+            "\x1F921"
+
+
+toName : Category -> String
+toName category =
+    case category of
+        Legitimate ->
+            "legítimo"
+
+        FakeNews ->
+            "fake news"
+
+        ClickBait ->
+            "click bait"
+
+        ExtremelyBiased ->
+            "tendencioso"
+
+        Satire ->
+            "Sátira"
--- src/StoryVotes.elm
+++ src/StoryVotes.elm
@@ -1,15 +1,18 @@
 port module StoryVotes exposing (..)
 
+import Category exposing (Category)
 import Element exposing (..)
 import Element.Attributes exposing (..)
 import Helpers exposing (onClickStopPropagation)
 import Html exposing (Html)
 import Html.Attributes
+import Http exposing (encodeUri)
+import Json.Decode
 import Stylesheet exposing (..)
 
 
 type alias Model =
-    { url : String }
+    { url : String, votes : List VoteCount }
 
 
 type alias Flags =
@@ -18,14 +21,45 @@ type alias Flags =
 
 type Msg
     = OpenFlagPopup
+    | VotesResponse (Result Http.Error (List VoteCount))
+    | AddVote { categoryId : Int }
 
 
 port openFlagPopup : { url : String } -> Cmd msg
 
 
+port addVote : ({ categoryId : Int } -> msg) -> Sub msg
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    addVote AddVote
+
+
 init : Flags -> ( Model, Cmd Msg )
 init flags =
-    ( { url = flags.url }, Cmd.none )
+    ( { url = flags.url, votes = [] }, getVotes flags.url )
+
+
+type alias VoteCount =
+    { category : Category, count : Int }
+
+
+decodeVoteCount : Json.Decode.Decoder (List VoteCount)
+decodeVoteCount =
+    Json.Decode.list
+        (Json.Decode.map2 VoteCount
+            (Json.Decode.field "category_id" Json.Decode.int
+                |> Json.Decode.map Category.fromId
+            )
+            (Json.Decode.field "count" Json.Decode.int)
+        )
+
+
+getVotes : String -> Cmd Msg
+getVotes url =
+    Http.get ("https://fake-news-detector-api.herokuapp.com/votes?url=" ++ encodeUri url) decodeVoteCount
+        |> Http.send VotesResponse
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -34,6 +68,38 @@ update msg model =
         OpenFlagPopup ->
             ( model, openFlagPopup { url = model.url } )
 
+        VotesResponse response ->
+            case response of
+                Ok votes ->
+                    ( { model | votes = votes }, Cmd.none )
+
+                Err err ->
+                    ( model, Cmd.none )
+
+        AddVote { categoryId } ->
+            let
+                category =
+                    Category.fromId categoryId
+
+                hasVote =
+                    List.filter (\voteCount -> voteCount.category == category) model.votes
+                        |> List.head
+
+                votes =
+                    if hasVote == Nothing then
+                        { category = category, count = 1 } :: model.votes
+                    else
+                        List.map
+                            (\voteCount ->
+                                if voteCount.category == category then
+                                    { voteCount | count = voteCount.count + 1 }
+                                else
+                                    voteCount
+                            )
+                            model.votes
+            in
+            ( { model | votes = votes }, Cmd.none )
+
 
 main : Program Flags Model Msg
 main =
@@ -41,7 +107,7 @@ main =
         { init = init
         , view = view
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
 
 
@@ -54,14 +120,30 @@ view model =
             , ( "z-index", "1" )
             ]
         ]
-        [ Element.layout stylesheet (votes model)
+        [ Element.layout stylesheet (viewVotes model)
         ]
 
 
-votes : Model -> Element Classes variation Msg
-votes model =
+viewVotes : Model -> Element Classes variation Msg
+viewVotes model =
     column NoStyle
         [ spacing 5, padding 5 ]
         [ button Button [ padding 4, onClickStopPropagation OpenFlagPopup ] (text "🏴 Sinalizar")
-        , el VoteCount [ padding 6 ] (text "\x1F916 60% click bait")
+        , row VoteCountItem
+            [ padding 6, spacing 5, height (px 26) ]
+            [ el VoteEmoji [ moveUp 5 ] (text "\x1F916")
+            , text "60%"
+            , text "click bait"
+            ]
+        , column NoStyle [ spacing 5 ] (List.map viewVote model.votes)
+        ]
+
+
+viewVote : VoteCount -> Element Classes variation Msg
+viewVote voteCount =
+    row VoteCountItem
+        [ padding 6, spacing 5, height (px 26) ]
+        [ el VoteEmoji [ moveUp 5 ] (text (Category.toEmoji voteCount.category))
+        , text (toString voteCount.count)
+        , text (Category.toName voteCount.category)
         ]
