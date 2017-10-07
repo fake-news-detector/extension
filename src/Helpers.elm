95c6a8741609eea3755ada4973b28078e9efd092
Rogerio Chaves <rogeriocfj@gmail.com>
2017-10-06 21:39:03 -0300

    Show better error message from API

--- src/Helpers.elm
+++ src/Helpers.elm
@@ -2,6 +2,7 @@ module Helpers exposing (..)
 
 import Element exposing (..)
 import Element.Events exposing (..)
+import Http exposing (Error(..))
 import Json.Decode
 
 
@@ -10,3 +11,22 @@ onClickStopPropagation msg =
     onWithOptions "click"
         { defaultOptions | stopPropagation = True, preventDefault = True }
         (Json.Decode.succeed msg)
+
+
+humanizeError : Http.Error -> String
+humanizeError error =
+    case error of
+        BadStatus data ->
+            data.body
+
+        Timeout ->
+            "Timeout: operação demorou tempo demais"
+
+        NetworkError ->
+            "Erro de rede: verifique sua conexão à internet"
+
+        BadPayload _ data ->
+            data.body
+
+        BadUrl error ->
+            error
