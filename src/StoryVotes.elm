eef4ab9e2beadd367f4525e28180967230b0749e
Rogerio Chaves <rogeriocfj@gmail.com>
2017-10-08 00:52:04 -0300

    Use new API to render robot's guesses

--- src/StoryVotes.elm
+++ src/StoryVotes.elm
@@ -1,19 +1,19 @@
 port module StoryVotes exposing (..)
 
 import Data.Category as Category exposing (Category)
-import Data.Votes as Votes exposing (VoteCount)
+import Data.Votes as Votes exposing (PeopleAndRobotVotes, PeopleVote)
 import Element exposing (..)
 import Element.Attributes exposing (..)
 import Helpers exposing (onClickStopPropagation)
 import Html exposing (Html)
 import Html.Attributes
-import Http exposing (encodeUri)
 import List.Extra
+import RemoteData exposing (..)
 import Stylesheet exposing (..)
 
 
 type alias Model =
-    { url : String, title : String, votes : List VoteCount }
+    { url : String, title : String, votes : WebData PeopleAndRobotVotes }
 
 
 type alias Flags =
@@ -22,7 +22,7 @@ type alias Flags =
 
 type Msg
     = OpenFlagPopup
-    | VotesResponse (Result Http.Error (List VoteCount))
+    | VotesResponse (WebData PeopleAndRobotVotes)
     | AddVote { categoryId : Int }
 
 
@@ -44,9 +44,10 @@ main =
 
 init : Flags -> ( Model, Cmd Msg )
 init flags =
-    ( { url = flags.url, title = flags.title, votes = [] }
-    , Votes.getVotes flags.url
-        |> Http.send VotesResponse
+    ( { url = flags.url, title = flags.title, votes = NotAsked }
+    , Votes.getVotes flags.url flags.title
+        |> RemoteData.sendRequest
+        |> Cmd.map VotesResponse
     )
 
 
@@ -62,12 +63,7 @@ update msg model =
             ( model, openFlagPopup { url = model.url, title = model.title } )
 
         VotesResponse response ->
-            case response of
-                Ok votes ->
-                    ( { model | votes = votes }, Cmd.none )
-
-                Err err ->
-                    ( model, Cmd.none )
+            ( { model | votes = response }, Cmd.none )
 
         AddVote { categoryId } ->
             let
@@ -77,15 +73,25 @@ update msg model =
                 isCategory voteCount =
                     voteCount.category == category
 
-                votes =
-                    if List.Extra.find isCategory model.votes == Nothing then
-                        { category = category, count = 1 } :: model.votes
-                    else
-                        List.Extra.updateIf isCategory
-                            (\voteCount -> { voteCount | count = voteCount.count + 1 })
-                            model.votes
+                peopleVotes =
+                    case model.votes of
+                        Success votes ->
+                            if List.Extra.find isCategory votes.people == Nothing then
+                                { category = category, count = 1 } :: votes.people
+                            else
+                                List.Extra.updateIf isCategory
+                                    (\voteCount -> { voteCount | count = voteCount.count + 1 })
+                                    votes.people
+
+                        _ ->
+                            [ { category = category, count = 1 } ]
+
+                updatedVotes =
+                    model.votes
+                        |> RemoteData.map (\votes -> { votes | people = peopleVotes })
+                        |> RemoteData.withDefault { robot = [], people = peopleVotes }
             in
-            ( { model | votes = votes }, Cmd.none )
+            ( { model | votes = Success updatedVotes }, Cmd.none )
 
 
 view : Model -> Html Msg
@@ -97,27 +103,58 @@ view model =
             , ( "z-index", "1" )
             ]
         ]
-        [ Element.layout stylesheet (viewVotes model)
+        [ Element.layout stylesheet (flagButtonAndVotes model)
         ]
 
 
-viewVotes : Model -> Element Classes variation Msg
-viewVotes model =
+flagButtonAndVotes : Model -> Element Classes variation Msg
+flagButtonAndVotes model =
     column NoStyle
         [ spacing 5, padding 5 ]
         [ button Button [ padding 4, onClickStopPropagation OpenFlagPopup ] (text "🏴 Sinalizar")
-        , row VoteCountItem
-            [ padding 6, spacing 5, height (px 26) ]
-            [ el VoteEmoji [ moveUp 5 ] (text "\x1F916")
-            , text "60%"
-            , text "click bait"
-            ]
-        , column NoStyle [ spacing 5 ] (List.map viewVote model.votes)
+        , case model.votes of
+            Success votes ->
+                viewVotes votes
+
+            Failure _ ->
+                el VoteCountItem [ padding 6 ] (text "erro ao carregar")
+
+            _ ->
+                empty
         ]
 
 
-viewVote : VoteCount -> Element Classes variation Msg
-viewVote voteCount =
+viewVotes : PeopleAndRobotVotes -> Element Classes variation Msg
+viewVotes votes =
+    column NoStyle
+        []
+        [ case Votes.bestRobotGuess votes.robot of
+            Just ( category, chance ) ->
+                row VoteCountItem
+                    [ padding 6, spacing 5, height (px 26) ]
+                    [ el VoteEmoji [ moveUp 5 ] (text "\x1F916")
+                    , text (toString chance ++ "%")
+                    , text (Category.toName category)
+                    ]
+
+            Nothing ->
+                empty
+        , column NoStyle [ spacing 5 ] (List.map viewPeopleVotes votes.people)
+        ]
+
+
+viewRobotVotes : PeopleVote -> Element Classes variation Msg
+viewRobotVotes voteCount =
+    row VoteCountItem
+        [ padding 6, spacing 5, height (px 26) ]
+        [ el VoteEmoji [ moveUp 5 ] (text (Category.toEmoji voteCount.category))
+        , text (toString voteCount.count)
+        , text (Category.toName voteCount.category)
+        ]
+
+
+viewPeopleVotes : PeopleVote -> Element Classes variation Msg
+viewPeopleVotes voteCount =
     row VoteCountItem
         [ padding 6, spacing 5, height (px 26) ]
         [ el VoteEmoji [ moveUp 5 ] (text (Category.toEmoji voteCount.category))
