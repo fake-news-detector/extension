667f058a6785c0fca52c81ebaa1a0f39cce406d1
Rogerio Chaves <rogeriocfj@gmail.com>
2017-10-08 00:52:04 -0300

    Use new API to render robot's guesses

--- src/Data/Votes.elm
+++ src/Data/Votes.elm
@@ -4,16 +4,43 @@ import Data.Category as Category exposing (..)
 import Http exposing (encodeUri)
 import Json.Decode
 import Json.Encode
+import List.Extra
 
 
-type alias VoteCount =
+type alias PeopleVote =
     { category : Category, count : Int }
 
 
-decodeVoteCount : Json.Decode.Decoder (List VoteCount)
-decodeVoteCount =
+type alias RobotVote =
+    { category : Category, chance : Float }
+
+
+type alias PeopleAndRobotVotes =
+    { robot : List RobotVote, people : List PeopleVote }
+
+
+decodePeopleAndRobotVotes : Json.Decode.Decoder PeopleAndRobotVotes
+decodePeopleAndRobotVotes =
+    Json.Decode.map2 PeopleAndRobotVotes
+        (Json.Decode.field "robot" decodeRobotVotes)
+        (Json.Decode.field "people" decodePeopleVotes)
+
+
+decodeRobotVotes : Json.Decode.Decoder (List RobotVote)
+decodeRobotVotes =
     Json.Decode.list
-        (Json.Decode.map2 VoteCount
+        (Json.Decode.map2 RobotVote
+            (Json.Decode.field "category_id" Json.Decode.int
+                |> Json.Decode.map Category.fromId
+            )
+            (Json.Decode.field "chance" Json.Decode.float)
+        )
+
+
+decodePeopleVotes : Json.Decode.Decoder (List PeopleVote)
+decodePeopleVotes =
+    Json.Decode.list
+        (Json.Decode.map2 PeopleVote
             (Json.Decode.field "category_id" Json.Decode.int
                 |> Json.Decode.map Category.fromId
             )
@@ -21,9 +48,9 @@ decodeVoteCount =
         )
 
 
-getVotes : String -> Http.Request (List VoteCount)
-getVotes url =
-    Http.get ("https://fake-news-detector-api.herokuapp.com/votes?url=" ++ encodeUri url) decodeVoteCount
+getVotes : String -> String -> Http.Request PeopleAndRobotVotes
+getVotes url title =
+    Http.get ("https://fake-news-detector-api.herokuapp.com/votes?url=" ++ encodeUri url ++ "&title=" ++ encodeUri title) decodePeopleAndRobotVotes
 
 
 encodeNewVote : String -> String -> String -> Category -> Json.Encode.Value
@@ -41,3 +68,9 @@ postVote uuid url title category =
     Http.post "https://fake-news-detector-api.herokuapp.com/vote"
         (Http.jsonBody (encodeNewVote uuid url title category))
         (Json.Decode.succeed ())
+
+
+bestRobotGuess : List RobotVote -> Maybe ( Category, Int )
+bestRobotGuess robotVotes =
+    List.Extra.maximumBy (\robotVote -> robotVote.chance) robotVotes
+        |> Maybe.map (\vote -> ( vote.category, round (vote.chance * 100) ))
