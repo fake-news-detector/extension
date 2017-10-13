a7fa273ebf0e5b92a7238a9345971a7f0a814180
Rogerio Chaves <rogeriocfj@gmail.com>
2017-10-13 01:30:22 -0300

    Extract duplicated parts from fb and twitter injectors

--- src/injectors/twitter.js
+++ src/injectors/twitter.js
@@ -1,30 +1,11 @@
+import { injectOnChange } from "./common";
 import { getExternalLinkTweet } from "./twitter/externalLinks";
 
-const findElementToRenderDetector = tweet => getExternalLinkTweet(tweet);
-
-const injectDetector = onInject => () =>
-  [...document.querySelectorAll(".tweet")]
-    .map(findElementToRenderDetector)
-    .filter(a => a)
-    .forEach(onInject);
-
-let injectTimeout;
-const injectOnFeedRefresh = onInject => {
-  const observer = new MutationObserver(function(mutations) {
-    clearTimeout(injectTimeout);
-    injectTimeout = setTimeout(injectDetector(onInject), 100);
-  });
-
-  observer.observe(document.querySelector("#timeline"), {
-    subtree: true,
-    childList: true
-  });
-};
+const elementsToBeInjected = () =>
+  [...document.querySelectorAll(".tweet")].map(getExternalLinkTweet);
 
 export default onInject => {
   if (!document.location.href.match("twitter.com")) return false;
 
-  setInterval(injectDetector(onInject), 5000);
-  injectOnFeedRefresh(onInject);
-  injectDetector(onInject)();
+  injectOnChange("#timeline", elementsToBeInjected, onInject);
 };
