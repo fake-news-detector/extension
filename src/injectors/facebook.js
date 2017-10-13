a2193fc8ec4b32f48f30102a7a67bf31e467d768
Rogerio Chaves <rogeriocfj@gmail.com>
2017-10-13 01:30:22 -0300

    Extract duplicated parts from fb and twitter injectors

--- src/injectors/facebook.js
+++ src/injectors/facebook.js
@@ -1,6 +1,12 @@
+import { injectOnChange } from "./common";
 import { getExternalLinkStory } from "./facebook/externalLinks.js";
 import { getPagePost } from "./facebook/pagePost.js";
 import { getSharePagePost } from "./facebook/sharePagePost.js";
+
+const markChecked = userStory => {
+  userStory.classList.add("fnd-checked");
+  return userStory;
+};
 
 export const notAdvertisement = userStory =>
   !!userStory.querySelector(".timestampContent");
@@ -12,37 +18,15 @@ const findElementToRenderDetector = userStory =>
   getPagePost(userStory) ||
   getSharePagePost(userStory);
 
-const markChecked = userStory => {
-  userStory.classList.add("fnd-checked");
-  return userStory;
-};
-
-const injectDetector = onInject => () =>
+const elementsToBeInjected = () =>
   [...document.querySelectorAll(".fbUserStory:not(.fnd-checked)")]
     .map(markChecked)
     .filter(notAdvertisement)
     .filter(notNested)
-    .map(findElementToRenderDetector)
-    .filter(a => a)
-    .forEach(onInject);
-
-let injectTimeout;
-const injectOnFeedRefresh = onInject => {
-  const observer = new MutationObserver(function(mutations) {
-    clearTimeout(injectTimeout);
-    injectTimeout = setTimeout(injectDetector(onInject), 100);
-  });
-
-  observer.observe(document.querySelector("#content_container"), {
-    subtree: true,
-    childList: true
-  });
-};
+    .map(findElementToRenderDetector);
 
 export default onInject => {
   if (!document.location.href.match("facebook.com")) return false;
 
-  setInterval(injectDetector(onInject), 5000);
-  injectOnFeedRefresh(onInject);
-  injectDetector(onInject)();
+  injectOnChange("#content_container", elementsToBeInjected, onInject);
 };
