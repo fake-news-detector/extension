056ba50e4e06e436b0917be9a28d25bf19ed25dd
Rogerio Chaves <rogeriocfj@gmail.com>
2017-10-18 01:14:14 -0200

    Add extensionPopup for classifying any site

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import { StoryVotes } from "./StoryVotes.elm";
 import { FlagPopup } from "./FlagPopup.elm";
 import facebookInjector from "./injectors/facebook";
 import twitterInjector from "./injectors/twitter";
+import {
+  extensionPopupInjector,
+  isExtensionPopup
+} from "./injectors/extensionPopup";
 import uuidv4 from "uuid/v4";
 
 const uuid = localStorage.getItem("uuid") || uuidv4();
 localStorage.setItem("uuid", uuid);
 
-const popup = FlagPopup.fullscreen({ uuid });
+const popup = FlagPopup.fullscreen({ uuid, isExtensionPopup });
 
 let storyVotes = {};
 const onInject = ({ elem, url, title }) => {
@@ -18,7 +22,11 @@ const onInject = ({ elem, url, title }) => {
   newNode.style.position = "relative";
   elem.insertBefore(newNode, elem.firstChild);
 
-  const storyVoting = StoryVotes.embed(newNode, { url, title });
+  const storyVoting = StoryVotes.embed(newNode, {
+    url,
+    title,
+    isExtensionPopup
+  });
   storyVoting.ports.openFlagPopup.subscribe(popup.ports.openFlagPopup.send);
   storyVotes[url] = storyVoting;
 };
@@ -29,3 +37,4 @@ popup.ports.broadcastVote.subscribe(({ url, categoryId }) => {
 
 facebookInjector(onInject);
 twitterInjector(onInject);
+extensionPopupInjector(onInject);
