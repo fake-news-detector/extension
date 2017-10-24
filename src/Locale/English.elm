f88897dd464db9d0d05c265377ea3107ca59d2e6
Jouderian Nobre <jouderianjr@gmail.com>
2017-10-24 00:45:28 -0300

    Add missing translation values

--- src/Locale/English.elm
+++ src/Locale/English.elm
@@ -22,37 +22,37 @@ translate localeValue =
             "Legitimate"
 
         LegitimateDescription ->
-            ""
+            "Honest content, does not try to fool anyone"
 
         FakeNews ->
             "Fake News"
 
         FakeNewsDescription ->
-            ""
+            "Tries to fool the reader and spreads rumors"
 
         ClickBait ->
             "Click Bait"
 
         ClickBaitDescription ->
-            ""
+            "Fancy title, does not explain anything about the news just to get more clicks"
 
         ExtremelyBiased ->
             "Extremely Biased"
 
         ExtremelyBiasedDescription ->
-            ""
+            "Shows only one side of the story, some points of the news would be exaggerated"
 
         Satire ->
             "Satire"
 
         SatireDescription ->
-            ""
+            "Fake content on purpose, just to make humor"
 
         NotNews ->
             "Not News"
 
         NotNewsDescription ->
-            ""
+            "Meme, personal content or anything that is not news"
 
         Verified ->
             "(verified)"
