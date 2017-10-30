3a108cc51b7bea563bf65a8b13f7c66dc3672ab2
Rogerio Chaves <rogeriocfj@gmail.com>
2017-10-29 23:58:41 -0200

    Add spanish language and words

---
+++ src/Locale/Spanish.elm
@@ -0,0 +1,73 @@
+module Locale.Spanish exposing (..)
+
+import Locale.Words exposing (LocaleKey(..))
+
+
+translate : LocaleKey -> String
+translate localeValue =
+    case localeValue of
+        Loading ->
+            "Cargando..."
+
+        ReportContent ->
+            "Señalar contenido"
+
+        ReportQuestion ->
+            "¿Cuál de las siguientes opciones define mejor este contenido?"
+
+        ReportButton ->
+            "Señalar"
+
+        Legitimate ->
+            "Auténtico"
+
+        LegitimateDescription ->
+            "Contenido verdadero, no intenta engañar a nadie, de ninguna manera"
+
+        FakeNews ->
+            "Fake News"
+
+        FakeNewsDescription ->
+            "Noticia falsa, engaña al lector, difunde rumores"
+
+        ClickBait ->
+            "Click Bait"
+
+        ClickBaitDescription ->
+            "Título no explica la noticia completa de propósito sólo para ganar clics"
+
+        ExtremelyBiased ->
+            "Extremadamente Tienencioso"
+
+        ExtremelyBiasedDescription ->
+            "Sólo muestra un lado de la historia, interpreta de forma exagerada algunos puntos, sin ponderación con otros"
+
+        Satire ->
+            "Sátira"
+
+        SatireDescription ->
+            "Contenido intencionalmente falso, con fines humorísticos"
+
+        NotNews ->
+            "No es noticia"
+
+        NotNewsDescription ->
+            "Meme, contenido personal o cualquier otra cosa no periodística"
+
+        Verified ->
+            "(verificado)"
+
+        FlagReportButton ->
+            "🏴 Señalar"
+
+        InvalidUrlError ->
+            "Url inválida: "
+
+        LoadingError ->
+            "error al cargar"
+
+        TimeoutError ->
+            "Timeout: operación tomó demasiado tiempo"
+
+        NetworkError ->
+            "Error de red: compruebe su conexión a Internet"
